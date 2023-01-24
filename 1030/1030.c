ead2d78c20aa97aabb8c3cc9b031d7863f2c679c
lowannann <jajoanne.chi88@gmail.com>
2023-01-24 18:01:04 +0800

    added 1030

---
+++ 1030/1030.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define N 5
+
+int i;
+int ary[N];
+
+int main(){
+
+    for(i=0;i<N;i++){
+        scanf("%d",&ary[i]);
+    }
+
+    for(i=0;i<N;i++){
+        printf("%d\n",ary[i]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
